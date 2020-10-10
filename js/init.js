5817c090a76f279633ff8e24c49967c117a34d08
Héber Silvério <hebersilverio@gmail.com>
2020-10-10 19:13:27 -0300

    final

--- js/init.js
+++ js/init.js
@@ -514,7 +514,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Músico", "Administrador", "Programador Júnior", "Escritor", "Palestrante"],
+			strings: ["Programador Júnior","Administrador", "Músico", "Escritor", "Palestrante"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
